73cc55333f0fa468f1b47df26ea05e367f8a4946
Abdulazizaisari2 <Abdulazizaisari06@gmail.com>
2025-07-20 14:43:11 +0400

    feat: Initial project setup with main class

---
+++ ScientificCalculator.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class ScientificCalculator {
+    public static void main(String[] args) {
+        System.out.println("Hello, Scientific Calculator!");
+    }
+}
